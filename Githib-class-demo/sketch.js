3fb1705c26d6eacbb2029f6337c317a8f7f4df84
alexvatajelu <alexvatajelu@hotmail.com>
2025-12-09 16:54:21 +0000

    circle2
    trying again

---
+++ Githib-class-demo/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(800, 600);
+}
+
+function draw() {
+    background(220);
+}
+
+
+//heya, i need help!! can we draw a circle???? thanks!!!
+
+//heloooo, im trying again
